7b2639cbbad7491a7ad43264a9cc6a5f1a8d92cf
Bjørn Hansen <55191733+bh1995@users.noreply.github.com>
2023-10-29 12:06:12 +0000

    c

--- src/app/App.js
+++ src/app/App.js
@@ -10,21 +10,27 @@ import Profile from "../components/Profile";
 import EditProfileForm from "../components/EditProfileForm";
 import Root from "../components/Root";
 
-import { Route } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route, Link, NavLink } from 'react-router-dom';
 
 import "./App.css";
 
-const router = 
+const router = createBrowserRouter(createRoutesFromElements(
   /* Wrap this Root Route to create Router here */
   <Route path="/" element={ <Root/> }>
-    {/* Add Routes here! */}
+    {/* Add Routes here! */} 
+    <Route path="/about" element={ <About/> } />  {/* nested route, About: /about */}
+    <Route path="/sign-up" element={ <SignUp/> } />  {/* nested route, SignUp: /sign-up */}
+    <Route path="/articles" element={ <Articles/> } />  {/* nested route, Articles: /articles */}
+    <Route path="/categories" element={ <Categories/> } />  {/* nested route, Categories: /categories */}
+    <Route path="/profile" element={ <Profile/> } />  {/* nested route, Profile: /profile */}
   </Route>
+));
 
 function App() {
   return (
     <>
       { /* Replace below and add Router Provider*/}
-      <p>REPLACE ME WITH A ROUTER PROVIDER</p>
+      <RouterProvider router={ router } />
     </>
   );
 }
--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import { useSelector } from "react-redux";
 import { selectArticles, filterArticles } from "../features/articles/articlesSlice";
 import Search from "./Search";
 
 // Import Link and useSearchParams from React Router
 
-export default function Articles () {
+export default function Articles() {
   const articles = useSelector(selectArticles);
 
   // Grab URLSearchParams object from useSearchParams hook
-  
+
   // Get the queryParams from object returned from useSearchParams and set to `title`
   const title = '';
 
@@ -19,12 +20,13 @@ export default function Articles () {
     <main>
       <h1>Articles</h1>
       <ul>
-        { filteredArticles.map(article => (
+        {filteredArticles.map(article => (
           <li key={article.slug}>
             {/* Replace these a tags! */}
-            <a href={`${article.slug}`}>
+            {/* <a href={`${article.slug}`}>
               {article.title}
-            </a>
+            </a> */}
+            <Link to={`${article.slug}`}>{article.title}</Link>
           </li>
         ))}
       </ul>
